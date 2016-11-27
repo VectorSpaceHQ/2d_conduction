63240c7dd63e15cef97b7caa95c9230a8d3c2013
Jordan Goulder <jordan.goulder@gmail.com>
2016-11-26 22:02:34 -0500

    Fix array bounds errors and add print out initial conditions

--- conduction.c
+++ conduction.c
@@ -1,6 +1,7 @@
 // 2D, Transient Conduction Solver for Hackaday 1Kb challenge.
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 
@@ -18,22 +19,31 @@ int main(){
 
 
     // initial conditions
-    for (int i=0; i<=N; i++){
-        T[i][0] = 40.0;
-    }
-    for (int i=0; i<=N; i++){
-        T[i][N] = 50.0;
-    }
-    for (int i=0; i<=N; i++){
-        T[0][i] = 50.0;
-    }
-    for (int i=0; i<=N; i++){
-        T[N][i] = 50.0;
-    }
-    for (int i=1; i<N; i++){
-        for (int j=1; j<N; j++){
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
             T[i][j] = 40.0;
         }
+    }
+    for (int i=0; i<N; i++){
+        T[i][0] = 40.0;
+    }
+    for (int i=0; i<N; i++){
+        T[i][N-1] = 50.0;
+    }
+    for (int i=0; i<N; i++){
+        T[0][i] = 50.0;
+    }
+    for (int i=0; i<N; i++){
+        T[N-1][i] = 50.0;
+    }
+
+    cout << endl << "Initial conditions" << endl;
+    cout << setprecision(2);
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
+            cout << T[i][j]<< ", " ;
+        }
+        cout << endl;
     }
 
 
@@ -46,8 +56,8 @@ int main(){
             }
         }
 
-        for (int j=1; j<N; j++){
-            for (int i=1; i<N; i++){
+        for (int j=1; j<N-1; j++){
+            for (int i=1; i<N-1; i++){
                 T[i][j] = ((alpha * dt) / dx_squared)
                     * (T_old[i+1][j] + T_old[i-1][j] + T_old[i][j-1] + T_old[i][j+1] - 4*T_old[i][j])
                     + T_old[i][j];
@@ -57,8 +67,9 @@ int main(){
 
 
     // display matrix just for debugging
-    for (int i=0; i<=N; i++){
-        for (int j=0; j<=N; j++){
+    cout << endl << "Final conditions" << endl;
+    for (int i=0; i<N; i++){
+        for (int j=0; j<N; j++){
             cout << T[i][j]<< ", " ;
         }
         cout << endl;
