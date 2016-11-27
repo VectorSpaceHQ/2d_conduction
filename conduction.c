8232283f85fa346a7e3787550cd0d0b5ffee86c1
Adam S <spontarelliam@gmail.com>
2016-11-26 21:05:53 -0500

    reorg. Fixed initial conditions

--- conduction.c
+++ conduction.c
@@ -1,51 +1,68 @@
 // 2D, Transient Conduction Solver for Hackaday 1Kb challenge.
 
 #include <iostream>
-#include <string>
 using namespace std;
 
+
 int main(){
-    float T[28][28] = {0.0};
-    float T_old[28][28] = {0.0};
-    float alpha = 1.0E-3;
-    float dt = 1.0;
-    float dx = 0.1;
-    float dx_squared = dx * dx;
+    int N = 28;
+    int tmax = 1000;
 
-    // initial condition
-    T[0][0] = 20;
-    T[0][28] = 50;
-    T[28][28] = 50;
-    T[28][0] = 50;
+    float T[N][N];
+    float T_old[N][N];
+    float alpha = 1.0E-2;
+    // time step size
+    float dt = 0.1;
+    // distance between LEDs, squared
+    float dx_squared = 1.0 * 1.0;
 
-    cout << T[12][13] << endl;
 
-    for (int t=0; t<200; t++){
+    // initial conditions
+    for (int i=0; i<=N; i++){
+        T[i][0] = 40.0;
+    }
+    for (int i=0; i<=N; i++){
+        T[i][N] = 50.0;
+    }
+    for (int i=0; i<=N; i++){
+        T[0][i] = 50.0;
+    }
+    for (int i=0; i<=N; i++){
+        T[N][i] = 50.0;
+    }
+    for (int i=1; i<N; i++){
+        for (int j=1; j<N; j++){
+            T[i][j] = 40.0;
+        }
+    }
+
+
+    for (int t=0; t<tmax; t++){
 
         // Please optimize this
-        /* memcpy(T_old, T, sizeof (int) * 28 * 28); */
-        for (int i=1; i<28; i++){
-            for (int j=1; j<28; j++){
+        for (int i=0; i<=N; i++){
+            for (int j=0; j<=N; j++){
                 T_old[i][j] = T[i][j];
             }
         }
 
-        for (int i=1; i<28; i++){
-            for (int j=1; j<28; j++){
-                T[i][j] = ((alpha * dt) / (dx_squared)) * (T_old[i+1][j] + T_old[i-1][j] + T_old[i][j-1] + T_old[i][j+1]) - (4 * alpha * dt)/(dx_squared) * T_old[i][j] + T_old[i][j];
-
+        for (int j=1; j<N; j++){
+            for (int i=1; i<N; i++){
+                T[i][j] = ((alpha * dt) / dx_squared)
+                    * (T_old[i+1][j] + T_old[i-1][j] + T_old[i][j-1] + T_old[i][j+1] - 4*T_old[i][j])
+                    + T_old[i][j];
             }
         }
     }
 
-    // just for debugging
-    for (int i=1; i<28; i++){
-        for (int j=1; j<28; j++){
+
+    // display matrix just for debugging
+    for (int i=0; i<=N; i++){
+        for (int j=0; j<=N; j++){
             cout << T[i][j]<< ", " ;
         }
         cout << endl;
     }
-
 
     return 0;
 }
