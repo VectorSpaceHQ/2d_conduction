451d083f7e9ecdd0182c8191f7b7bc152431f07a
Adam Spontarelli <adam@vector-space.org>
2016-12-06 22:53:28 -0500

    zigzag needed a j multiplier

--- conduction.c
+++ conduction.c
@@ -118,20 +118,19 @@ int main()
         rgb_matrix_start_frame();
 
         /* uint8_t iforward = -1; */
-        /* uint8_t kforward = -1; */
+        /* uint8_t kforward = 1; */
         /* uint8_t column = 0; */
+        // uint8_t row = 1;
         /* for (uint8_t k = 0; k < 12; k++) { */
         /*   if (k % 4 == 0){ */
         /*     kforward = kforward * -1; */
+        //     row += 1;
         /*   } */
         /*   for (uint8_t j = 0; j < 8; j++) { */
         /*     iforward = iforward * -1; */
         /*     for (uint8_t i = 0; i < 8; i++) { */
         /*       // convert to color */
-        /*       if (kforward == -1){ */
-        /*         i = 32-i */
-        /*       } */
-        /*       xyz = T(i * iforward * (column+1), j); */
+        /*       xyz = T(i * iforward * (column+1), j * row); */
         /*       rgb_matrix_send_pixel(0, 0, 0); */
         /*     } */
         /*   } */
