2f86a3cdeaf5c14e1fb9dcd72837194ed0ce94be
Jordan Goulder <jordan.goulder@gmail.com>
2016-12-12 20:59:19 -0500

    Fix conduction Octave simulation

--- conduction_explicit.m
+++ conduction_explicit.m
@@ -1,36 +1,36 @@
 clear, clc;
 more off;
 
-Nx = 8;
-Ny = 8;
+Nx = 32;
+Ny = 24;
 
 Ttop = 0.5;
 Tleft = 0.5;
 Tbottom = 1.0;
 Tright = 0.5;
 
-Nmax = 10;
+Nmax = 1000;
 
 
-T = zeros(Nx,Ny);
+T = zeros(Ny,Nx);
 
 Tavg = (Ttop + Tleft + Tbottom + Tright) / 4;
 
-T(:,:) = Tavg;
+#T(:,:) = Tavg;
 
-T(1,:) = Tleft;
-T(Nx,:) = Tright;
-T(:,1) = Ttop;
-T(:,Ny) = Tbottom;
+T(1,:) = Ttop;
+T(Ny,:) = Tbottom;
+T(:,1) = Tleft;
+T(:,Nx) = Tright;
 
 colormap(jet)
 
-tao = 0.01;
+tao = 0.08;
 
 for n = 1:Nmax
 
-  contourf([1:Nx], [1:Ny], rot90(T));
-  pause(0.1)
+  #contourf([1:Nx], [1:Ny], rot90(T'));
+  #pause(0.001)
 
   
   for i = 2:Nx-1
@@ -40,17 +40,21 @@ for n = 1:Nmax
   Told(1) = T(2,1);
   Told(Nx) = T(2,Nx);
   
+  #Told = T;
+  
   for j = 2:Ny-1
     for i = 2:Nx-1      
       
-      t = ((Told(i) + T(j+1, i) + Told(i-1) + T(j,i+1)) - (T(j,i) / 4)) + T(j,i);
-      
+      #t = tao * ((Told(j-1,i) + Told(j+1, i) + Told(j,i-1) + Told(j,i+1)) - (Told(j,i) * 4)) + Told(j,i);
+      t = tao * ((Told(i) + T(j+1, i) + Told(i-1) + T(j,i+1)) - (T(j,i) * 4)) + T(j,i);
       Told(i) = T(j,i);
       
-      T(j,i) = tao * t;
+      T(j,i) = t;
     end
     
     Told(1) = T(j+1,1);
     Told(Nx) = T(j+1,Nx);
   end
 end
+
+contourf([1:Nx], [1:Ny], rot90(T'));
